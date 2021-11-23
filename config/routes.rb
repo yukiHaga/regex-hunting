2669c3ea66751dc72b4623a2a18e411be7f1e210
yukiHaga <oceansthirteen7510@gmail.com>
2021-11-23 15:01:26 +0900

    Add: バックエンドのルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      get 'start', to: 'game_managements#start'
+      post 'finish', to: 'game_managements#finish'
+
+      resources :user, only: %i(create update) do
+        get 'my-page', to: 'my_pages#index'
+        get 'ranking', to: 'rankings#index'
+        get 'percent', to: 'percents#get_correct_percent'
+      end
+      post 'login', to: 'user_sessions#create'
+      delete 'logout', to: 'user_sessions#destroy'
+    end
+  end
 end
