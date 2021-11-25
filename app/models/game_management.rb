36d866354cef5567aeec105d1249e063a55b2d0f
yukiHaga <oceansthirteen7510@gmail.com>
2021-11-25 09:36:05 +0900

    Fix: GameManagementモデルのenumを修正

--- app/models/game_management.rb
+++ app/models/game_management.rb
@@ -8,7 +8,7 @@ class GameManagement < ApplicationRecord
   has_many :incorrect_questions_per_game, through: :incorrect_per_game
 
   enum difficulty_level: { 初級: 0, 中級: 1, 上級: 2 }
-  enum game_result: { Win: 0, Lose: 1 }
+  enum game_result: { progress: 0, win: 1, lose: 2 }
   validates :difficulty_level, :game_result, :result_time, :play_date, :user_id, presence: true
   # Userモデルを作るまでは、以下のバリデーションをコメンアウトする。
   # validates :user, presence: true, if: -> { user_id.present? }
