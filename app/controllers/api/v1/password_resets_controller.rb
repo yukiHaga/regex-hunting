f6de8d9eea21e3a8be41bb296638e7be831ad576
yukiHaga <oceansthirteen7510@gmail.com>
2021-11-27 23:57:51 +0900

    Add: PasswordResetsコントローラを作成しました

--- app/controllers/api/v1/password_resets_controller.rb
+++ app/controllers/api/v1/password_resets_controller.rb
@@ -1,10 +1,28 @@
 class Api::V1::PasswordResetsController < ApplicationController
   def create
+    user = User.find_by(email: params[:email])
+    user&.deliver_reset_password_instructions!
+    render json: {}, status: :ok
   end
 
   def edit
+    token = params[:id]
+    user = User.load_from_reset_password_token(token)
+    not_authenticated if user.blank?
+    render json: {}, status: :ok
   end
 
   def update
+    token = params[:id]
+    user = User.load_from_reset_password_token(token)
+
+    return not_authenticated if user.blank?
+
+    user.password_confirmation = params[:user][:password_confirmation]
+    if user.change_password(params[:user][:password])
+      render json: {}, status: :ok
+    else
+      render json: {}, status: :ok
+    end
   end
 end
