d5cab392d7b47c236ab6f12d6a4b0dc9ad6a8364
yukiHaga <oceansthirteen7510@gmail.com>
2021-11-28 23:08:23 +0900

    Add: game_managementsテーブルのresult_timeカラムをTime型に変更

--- db/migrate/20211122134323_create_game_managements.rb
+++ db/migrate/20211122134323_create_game_managements.rb
@@ -3,7 +3,7 @@ class CreateGameManagements < ActiveRecord::Migration[6.0]
     create_table :game_managements do |t|
       t.integer :difficulty_level, null: false, default: 0
       t.integer :game_result, null: false, default: 0
-      t.datetime :result_time, null: false, default: 0
+      t.time :result_time, null: false, default: 0
       t.date :play_date, null: false
       t.references :user, null: false, foreign_key: true
 
