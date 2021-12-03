ed3c46a9291a50e04a9e36ee467380517a19a42f
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-03 23:39:08 +0900

    Add: APIリクエストに関する定数とスタイル(色とフォントサイズ)に関する定数を定義

---
+++ frontend/src/style_constants.js
@@ -0,0 +1,15 @@
+// 色を決める際に、定義定義する。
+export const COLORS = {
+  MAIN: '#618A12',
+  SUB: '#F6F6DC'
+  BORDER: '#e2e2e2',
+  SUB_TEXT: '#545454',
+  FONT_STROKE: '#030002',
+  RED: 'FF6347',
+  BLUE: '0066B3'
+}
+
+// フォントサイズを決める際に、適宜定義する。
+export const FONT_SIZE = {
+  HEADER_TITLE: '36px'
+}
