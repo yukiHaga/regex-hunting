e092ebf2bd1acde91f8d4888082e30fc5ef97567
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-05 08:05:26 +0900

    Add: コンポーネントからAPI関数を呼び出してみる

--- frontend/src/containers/LandingPages.jsx
+++ frontend/src/containers/LandingPages.jsx
@@ -1,6 +1,25 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 
-export const LandingPages = () => {
+// ログイン関係のAPIコール関数
+import { postUserSession, } from '../apis/login'; 
+// deleteUserSession 
+
+export const LandingPages = () => { 
+
+  // ユーザーをログインさせる。
+  useEffect(() => {
+    postUserSession({
+      user: {
+        email: 'glen@stroman.com',
+        password: '3150test' 
+      }
+    })
+    .then((data) => {
+      console.log(data);
+      console.log("ログインが成功した");
+    })
+  }, []);
+
   return (
     <>
       LPページ
--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -2,7 +2,7 @@ const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
 
 // ゲーム関係
 export const gameManagementsStart = `${DEFAULT_API_LOCALHOST}/start`;
-export const gameManagementsStart = `${DEFAULT_API_LOCALHOST}/finish`;
+export const gameManagementsFinish = `${DEFAULT_API_LOCALHOST}/finish`;
 
 // 会員登録
 export const usersCreate = `${DEFAULT_API_LOCALHOST}/users`;
@@ -16,13 +16,13 @@ export const rankings = `${DEFAULT_API_LOCALHOST}/ranking`;
 
 // ユーザー関係
 export const myPages = (user_id) => 
-  `${DEFAULT_API_LOCALHOST}/users/${user_Id}/my-page`;
+  `${DEFAULT_API_LOCALHOST}/users/${user_id}/my-page`;
 export const percents = (user_id) => 
-  `${DEFAULT_API_LOCALHOST}/users/${user_Id}/percent`;
+  `${DEFAULT_API_LOCALHOST}/users/${user_id}/percent`;
 export const accountSettings = (user_id) => 
-  `${DEFAULT_API_LOCALHOST}/users/${user_Id}/account-settings`;
+  `${DEFAULT_API_LOCALHOST}/users/${user_id}/account-settings`;
 export const titleSettings = (user_id) => 
-  `${DEFAULT_API_LOCALHOST}/users/${user_Id}/title-settings`;
+  `${DEFAULT_API_LOCALHOST}/users/${user_id}/title-settings`;
 
 // パスワードリセット関係
 export const passwordResetsCreate = `${DEFAULT_API_LOCALHOST}/password_resets`;
