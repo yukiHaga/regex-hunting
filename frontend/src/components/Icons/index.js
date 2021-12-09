7a98ebafbcff0f809eecc2f49617bcc4f0c82ca5
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-09 09:58:02 +0900

    Add: src/components/Icons/index.jsにアイコンの定数を定義

---
+++ frontend/src/components/Icons/index.js
@@ -0,0 +1,3 @@
+export { default as GoogleIcon } from '@material-ui/icons/Google';
+export { default as TwitterIcon } from '@material-ui/icons/Twitter';
+export { default as GitHubIcon } from '@material-ui/icons/GitHub';
