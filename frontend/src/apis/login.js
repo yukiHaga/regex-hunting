2e0b3b022f2317b0c5a9d93666ac4a90163cbc5e
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-12 16:51:29 +0900

    Add: axios.postにcookieを含めるための値を指定した

--- frontend/src/apis/login.js
+++ frontend/src/apis/login.js
@@ -2,17 +2,19 @@ import axios from 'axios';
 import { userSessionsCreate, userSessionsDestroy } from '../urls/index';
 
 // ログインするためのAPIコール関数
+// postの第3引数にwithCredentials: trueを指定することで、
+// API(Rails)と通信する際にデータにcookieを含めることができる
 export const postUserSession = async ({user: {email, password}}) => {
   try {
     const response = await axios.post(userSessionsCreate,
       {
         email: email,
         password: password
-      }
+      },
+      { withCredentials: true }
     );
     return response.data;
   } catch(e) {
-    console.log(e.response);
     throw e;
   }
 };
