7b8e7a98a9fcbbc187c68928d07a211fe313ee0c
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-13 13:37:24 +0900

    Fix: マイページが正常に動くように修正した

--- frontend/src/containers/MyPages.jsx
+++ frontend/src/containers/MyPages.jsx
@@ -1,9 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 
 // Presentational Components
 import { Header } from '../components/Header.jsx';
 
+// Contextオブジェクト
+import { UserContext } from "../context/UserProvider.js";
+
 export const MyPages = () => {
+
+  // useContext
+  // requestUserStateには、requestState, userState, errorsが格納されている
+  // userStateにはsessionとuserが格納されている
+  const { requestUserState } = useContext(UserContext);
+
   return (
     <>
       <Header />
