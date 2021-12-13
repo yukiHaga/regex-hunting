2b5a75ad99881739928bde2f9a1e9566d91b4eed
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-13 13:49:49 +0900

    Add: UserSessions#logged_in?アクションを追加

--- app/controllers/api/v1/user_sessions_controller.rb
+++ app/controllers/api/v1/user_sessions_controller.rb
@@ -23,7 +23,33 @@ class Api::V1::UserSessionsController < ApplicationController
 
   def destroy
     logout
-    render json: { session: false }, status: :ok
+    render json: {
+      session: false,
+      user: {}
+    }, status: :ok
+  end
+
+  def logged_in?
+    if current_user
+      render json: {
+        session: true,
+        user: {
+          name: current_user[:name],
+          rank: current_user[:rank],
+          total_experience: current_user[:total_experience],
+          maximum_experience_per_rank: current_user[:maximum_experience_per_rank],
+          temporary_experience: current_user[:temporary_experience],
+          open_rank: current_user[:open_rank],
+          active_title: current_user[:active_title],
+          email: current_user[:email]
+        }
+      }, status: :ok
+    else
+      render json: {
+        session: false,
+        user: {}
+      }, status: :ok
+    end
   end
 
   private
