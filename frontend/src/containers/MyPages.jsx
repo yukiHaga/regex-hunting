707ba985369eaab6dcb108b155a028fd354f421c
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-13 14:47:07 +0900

    Add: MyPagesにuseEffectを設定

--- frontend/src/containers/MyPages.jsx
+++ frontend/src/containers/MyPages.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useContext } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 
 // Presentational Components
 import { Header } from '../components/Header.jsx';
@@ -6,12 +6,46 @@ import { Header } from '../components/Header.jsx';
 // Contextオブジェクト
 import { UserContext } from "../context/UserProvider.js";
 
+// ログイン状態を確認するAPIコール関数
+import { checkLoginStatus } from '../apis/checkLoginStatus.js'; 
+
+// HTTP_STATUS_CODE
+import { HTTP_STATUS_CODE } from '../constants';
+
 export const MyPages = () => {
 
   // useContext
   // requestUserStateには、requestState, userState, errorsが格納されている
   // userStateにはsessionとuserが格納されている
-  const { requestUserState } = useContext(UserContext);
+  const { 
+    requestUserState, 
+    dispatch, 
+    requestUserActionTyps
+  } = useContext(UserContext);
+
+
+  useEffect(() => {
+    dispatch({ type: requestUserActionTyps.REQUEST });
+    checkLoginStatus().then((data) => {
+      dispatch({
+        type: requestUserActionTyps.REQUEST_SUCCESS,
+        payload: {
+          user: data.user
+        }
+      });
+    }).catch((e) => {
+      if(e.response.status === HTTP_STATUS_CODE.NOT_FOUND){
+        dispatch({
+          type: requestUserActionTyps.REQUEST_FAILURE,
+          payload: {
+            errors: e.response.data.errors
+          }
+        });
+      } else {
+        throw e;
+      }
+    })
+  }, []);
 
   return (
     <>
