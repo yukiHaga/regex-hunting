b611ea14507be8c63fff0535e5fbfdd2010986d0
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-22 14:30:32 +0900

    Add: Reactでイベントリスナーが使えることが分かった

--- frontend/src/components/Games/CodeBlock.jsx
+++ frontend/src/components/Games/CodeBlock.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styled from 'styled-components';
 
 // Colors
@@ -37,6 +37,28 @@ const CodeBlockTextWrapper = styled.div`
 `;
 
 export const CodeBlock = () => {
+
+  const [state, setState] = useState("");
+
+  const keyFunction = useCallback((e) => {
+    setState( prevState => {
+      const newState = prevState + e.key; 
+      return newState;
+    } );
+  }, []);
+
+  // useEffectの第一引数の副作用関数は、コンポーネントの初回レンダリング時、
+  // および、keyFunctionが変化したときに実行される
+  // クリーンアップ関数はコンポーネントがアンマウント, 副作用関数が再実行
+  // された時に実行される。クリーンアップ関数を副作用関数の戻り値として返すことで、
+  // コンポーネントがレンダリングされるたびにイベントの登録が重複するのを防ぐことができる
+  useEffect(() => {
+    document.addEventListener("keydown", keyFunction, false);
+    return () => {
+      document.removeEventListener("keydown", keyFunction, false);
+    }
+  }, [keyFunction]);
+
   return (
     <>
       <CodeBlockWrapper>
@@ -44,7 +66,7 @@ export const CodeBlock = () => {
           /
         </AnchorWrapper>
         <CodeBlockTextWrapper>
-         
+          {state} 
         </CodeBlockTextWrapper>
         <AnchorWrapper>
           /g
