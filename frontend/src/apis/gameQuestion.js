6675408b61e90b5a02b20204bcba146eff094f4c
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-25 11:33:10 +0900

    Add: APIコール関数のparamsの持たせ方を変えた

--- frontend/src/apis/gameQuestion.js
+++ frontend/src/apis/gameQuestion.js
@@ -4,13 +4,10 @@ import { gameManagementsStart } from '../urls/index';
 // ログインするためのAPIコール関数
 // postの第3引数にwithCredentials: trueを指定することで、
 // API(Rails)と通信する際にデータにcookieを含めることができる
-export const getGameStart = async ({ difficulty }) => {
+export const getGameStart = async (difficulty) => {
   try {
     const response = await axios.get(
-      gameManagementsStart,
-      {
-        difficulty: difficulty,
-      },
+      `${gameManagementsStart}?difficulty=${difficulty}`,
       { withCredentials: true }
     );
     axios.defaults.headers.common['X-CSRF-Token'] = response.headers['x-csrf-token'];
