f9497f3cdd1aa13d5a4c5e57cf34f7440eb7d2ff
yukiHaga <oceansthirteen7510@gmail.com>
2021-12-28 11:52:12 +0900

    Add: マッチした文字色と背景色を定数として定義した

--- frontend/src/style_constants.js
+++ frontend/src/style_constants.js
@@ -16,7 +16,9 @@ export const COLORS = {
   LIGHT_BLACK: '#666666',
   OCHER: '#D3AA6B',
   YELLOW: '#EBA12F',
-  GAGE_GRAY: '#BBB8AA'
+  GAGE_GRAY: '#BBB8AA',
+  WORD_BLUE: '#0066B3',
+  WORD_BACK: '#BFE4FF'
 }
 
 // フォントサイズを決める際に、適宜定義する。
