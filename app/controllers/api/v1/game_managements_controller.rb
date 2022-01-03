9f700afb25dab65671a8845b83d515b4dba664d9
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-03 11:21:22 +0900

    Fix: 問題を取得する個数を増やした

--- app/controllers/api/v1/game_managements_controller.rb
+++ app/controllers/api/v1/game_managements_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::GameManagementsController < ApplicationController
-  MIN_TIMES = 12
+  MIN_TIMES = 14
   skip_before_action :require_login
   skip_before_action :verify_authenticity_token, only: :start
   after_action :set_csrf_token_header, only: :start
