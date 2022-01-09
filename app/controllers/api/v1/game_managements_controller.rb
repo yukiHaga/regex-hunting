bb3c5b8f0cc548321f3d061eb445bc458f29a5a4
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-09 22:00:56 +0900

    Add: send_game_dataを追加した

--- app/controllers/api/v1/game_managements_controller.rb
+++ app/controllers/api/v1/game_managements_controller.rb
@@ -114,17 +114,6 @@ class Api::V1::GameManagementsController < ApplicationController
 
     # レンダリング
     render json: {
-      session: true,
-      user: {
-        name: current_user[:name],
-        rank: current_user[:rank],
-        total_experience: current_user[:total_experience],
-        maximum_experience_per_rank: current_user[:maximum_experience_per_rank],
-        temporary_experience: current_user[:temporary_experience],
-        open_rank: current_user[:open_rank],
-        active_title: current_user[:active_title],
-        email: current_user[:email]
-      },
       send_game_data: true
     }, status: :created
   end
