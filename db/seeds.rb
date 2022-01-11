a0db3610065859762c5354f5edbc8a512059f8b0
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-11 23:46:53 +0900

    Add: シードデータにhintを追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,6 +43,7 @@ Question.create!(
   sentence: "1, 5, 9の全てにマッチする正規表現を入力せよ。",
   target_sentence: "1, 5, 9",
   sample_answer: "\\d",
+  hint: "1桁の数字を表すメタ文字を使って、正規表現を作ってみましょう。",
   commentary: "\\dは1桁の数字を表します。[0-9]で書き換え可能です。",
   difficulty: "elementary"
 )
@@ -52,6 +53,7 @@ Question.create!(
   sentence: "10, 20, 30の全てにマッチする正規表現を入力せよ。",
   target_sentence: "10, 20, 30",
   sample_answer: "\\d{2}",
+  hint: "\\d{n}は、n桁の数字を表すメタ文字です。\\d{n}を使って、正規表現を作ってみましょう。",
   commentary: "\\d{2}は2桁の数字を表します。\\d+でマッチさせることもできます。",
   difficulty: "elementary"
 )
@@ -61,6 +63,7 @@ Question.create!(
   sentence: "100, 1000, 10000の全てにマッチする正規表現を入力せよ。",
   target_sentence: "100, 1000, 10000",
   sample_answer: "\\d{3,5}",
+  hint: "\\d{n,m}は、n桁~m桁の数字を表すメタ文字です。\\d{n,m}を使って、正規表現を作ってみましょう。",
   commentary: "\\d{3,5}は3桁~5桁の数字を表します。\\d+でマッチさせることもできます。",
   difficulty: "elementary"
 )
@@ -70,6 +73,7 @@ Question.create!(
   sentence: "1000, 10000, 100000にマッチする正規表現を入力せよ。",
   target_sentence: "10, 100, 1000, 10000, 100000",
   sample_answer: "\\d{4,6}",
+  hint: "\\d{n,m}は、n桁~m桁の数字を表すメタ文字です。\\d{n,m}を使って、正規表現を作ってみましょう。",
   commentary: "\\d{4,6}は4桁~6桁の数字を表します。\\d{4,}でマッチさせることもできます。",
   difficulty: "elementary"
 )
@@ -79,7 +83,8 @@ Question.create!(
   sentence: "A, B, Zの全てにマッチする正規表現を入力せよ。",
   target_sentence: "A, B, Z",
   sample_answer: "[ABZ]",
-  commentary: "[ABZ]はA, B, Zのどれか一文字を表します。[A-Z]でマッチさせることもできます。",
+  hint: "[ABC]は、A, B, Cのどれか1文字を表すメタ文字です。[ABC]を使って、正規表現を作ってみましょう。",
+  commentary: "[ABZ]はA, B, Zのどれか1文字を表します。[A-Z]でマッチさせることもできます。",
   difficulty: "elementary"
 )
 
@@ -88,7 +93,9 @@ Question.create!(
   sentence: "A, 9, Zの全てにマッチする正規表現を入力せよ。",
   target_sentence: "A, 9, Z",
   sample_answer: "[A9Z]",
-  commentary: "[A9Z]はA, 9, Zのどれか一文字を表します。\\wでマッチさせることもできます。",
+  hint: "[ABC]は、A, B, Cのどれか1文字を表すメタ文字です。[ABC]を使って、正規表現を作
+ってみましょう。",
+  commentary: "[A9Z]はA, 9, Zのどれか1文字を表します。\\wでマッチさせることもできます。",
   difficulty: "elementary"
 )
 
@@ -97,6 +104,7 @@ Question.create!(
   sentence: "A, a, 9の全てにマッチする正規表現を入力せよ。",
   target_sentence: "A, a, 9",
   sample_answer: "\\w",
+  hint: "大文字アルファベット1文字, 小文字アルファベット1文字, 数字1文字を表すメタ文字を使って、正規表現を作ってみましょう。 ",
   commentary: "\\wはa~z, A~Z, 0~9, _の中の1文字を表します。[a-zA-Z\\d]で書き換え可能です。",
   difficulty: "elementary"
 )
@@ -106,6 +114,7 @@ Question.create!(
   sentence: "aaa, aaaa, aaaaaにマッチする正規表現を入力せよ。",
   target_sentence: "aaa, aaaa, aaaaa, a, aa",
   sample_answer: "a{3,5}",
+  hint: "{n,m}は、直前の文字がn個以上、m個以下を表すメタ文字です。{n,m}を使って、正規表現を作ってみましょう。",
   commentary: "a{3,5}はaaa, aaaa, aaaaaを表します。",
   difficulty: "elementary"
 )
@@ -115,16 +124,18 @@ Question.create!(
   sentence: "X, y, Zの全てにマッチする正規表現を入力せよ。",
   target_sentence: "X, y, Z",
   sample_answer: "[XyZ]",
+  hint: "[ABC]は、A, B, Cのどれか1文字を表すメタ文字です。[ABC]を使って、正規表現を作ってみましょう。",
   commentary: "[XyZ]はX, y, Zを表します。\\w, [a-zA-Z]でマッチさせることもできます。",
   difficulty: "elementary"
 )
 
 # 10問目
 Question.create!(
-  sentence: "aaa, bbb, cccの全てにマッチする正規表現を入力せよ。",
-  target_sentence: "aaa, bbb, ccc",
-  sample_answer: "aaa|bbb|ccc",
-  commentary: "aaa|bbb|cccはaaa, bbb, cccを表します。[a-z]{3}でマッチさせることもできます。",
+  sentence: "xxx, yyy, zzzの全てにマッチする正規表現を入力せよ。",
+  target_sentence: "xxx, yyy, zzz",
+  sample_answer: "xxx|yyy|zzz",
+  hint: "aaa|bbb|cccは、aaa, bbb, cccを表すメタ文字です。aaa|bbb|cccを使って、正規表現を作ってみましょう。",
+  commentary: "xxx|yyy|zzzはxxx, yyy, zzzを表します。[a-z]{3}でマッチさせることもできます。",
   difficulty: "elementary"
 )
 
@@ -133,6 +144,7 @@ Question.create!(
   sentence: "aaa, baaa, caの全てにマッチする正規表現を入力せよ。",
   target_sentence: "aaa, baaa, ca",
   sample_answer: "aaa|baaa|ca",
+  hint: "aaa|bbb|cccは、aaa, bbb, cccを表すメタ文字です。aaa|bbb|cccを使って、正規表現を作ってみましょう。",
   commentary: "aaa|baaa|caはaaa, baaa, caを表します。[a-z]{2,4}で書き換え可能です。",
   difficulty: "elementary"
 )
@@ -140,9 +152,10 @@ Question.create!(
 # 12問目
 Question.create!(
   sentence: "aaa, 999, cccの全てにマッチする正規表現を入力せよ。",
-  target_sentence: "aaa, 999, ccc",
+  target_sentence: "aaa, 999, ccc, zzz",
   sample_answer: "aaa|999|ccc",
-  commentary: "aaa|999|cccはaaa, 999, cccを表します。\\w{3}でマッチさせることもできます。",
+  hint: "aaa|bbb|cccは、aaa, bbb, cccを表すメタ文字です。aaa|bbb|cccを使って、正規表現を作ってみましょう。",
+  commentary: "aaa|999|cccはaaa, 999, cccを表します。",
   difficulty: "elementary"
 )
 
@@ -151,16 +164,18 @@ Question.create!(
   sentence: "#, %, @の全てにマッチする正規表現を入力せよ。",
   target_sentence: "#, %, @",
   sample_answer: "[#%@]",
+  hint: "[ABC]は、A, B, Cのどれか1文字を表すメタ文字です。[ABC]を使って、正規表現を作ってみましょう。",
   commentary: "[#%@]は#, %, @を表します。",
   difficulty: "elementary"
 )
 
 # 14問目
 Question.create!(
-  sentence: "aaa, bbb, cccにマッチする正規表現を入力せよ。",
-  target_sentence: "aaa, bbb, ccc, ddd, eee",
-  sample_answer: "aaa|bbb|ccc",
-  commentary: "aaa|bbb|cccはaaa, 999, cccを表します。[abc]{3}でマッチさせることもできます。",
+  sentence: "xxx, yyy, zzzにマッチする正規表現を入力せよ。",
+  target_sentence: "xxx, yyy, zzz, ddd, eee",
+  sample_answer: "xxx|yyy|zzz",
+  hint: "aaa|bbb|cccは、aaa, bbb, cccを表すメタ文字です。aaa|bbb|cccを使って、正規表現を作ってみましょう。",
+  commentary: "xxx|yyy|zzzはxxx, yyy, zzzを表します。[xyz]{3}でマッチさせることもできます。",
   difficulty: "elementary"
 )
 
