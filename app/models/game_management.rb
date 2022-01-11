885e19d08064346b8d20b79ebb1be65b484e85fb
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-11 11:14:18 +0900

    Add: GameManagement.rbのコードを消した

--- app/models/game_management.rb
+++ app/models/game_management.rb
@@ -2,10 +2,8 @@ class GameManagement < ApplicationRecord
   belongs_to :user
   has_many :solved_questions, dependent: :destroy
   has_many :questions_per_game, through: :solved_questions, source: :question
-  has_many :correct_per_game, -> { where(judgement: :correct) }, class_name: :solved_question
-  has_many :correct_questions_per_game, through: :correct_per_game
-  has_many :incorrect_per_game, -> { where(judgement: :incorrect) }, class_name: :solved_question
-  has_many :incorrect_questions_per_game, through: :incorrect_per_game
+  has_many :correct_per_game, -> { where(judgement: :correct) }, class_name: :SolvedQuestion
+  has_many :incorrect_per_game, -> { where(judgement: :incorrect) }, class_name: :SolvedQuestion
 
   enum difficulty: { elementary: 0, intermediate: 1, advanced: 2 }
   enum game_result: { progress: 0, win: 1, lose: 2 }
