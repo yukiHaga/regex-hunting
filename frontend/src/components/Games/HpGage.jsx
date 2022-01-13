db048a6ae93017c68a0615b4fc193fedd4003b4f
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-13 10:18:08 +0900

    HPゲージとTimeゲージの空の部分に光沢を加えた

--- frontend/src/components/Games/HpGage.jsx
+++ frontend/src/components/Games/HpGage.jsx
@@ -40,6 +40,10 @@ const GageOuterWrapper = styled.div`
   width: 100%;
   background-color: ${COLORS.LIGHT_BLACK};
   border-radius: 10px;
+  background-image: -webkit-linear-gradient(transparent 0%,rgba(255,255,255,.3) 50%,transparent 50%,rgba(0,0,0,.1) 100%);
+  background-image:         linear-gradient(transparent 0%,rgba(255,255,255,.3) 50%,transparent 50%,rgba(0,0,0,.1) 100%);
+  box-shadow: 0 2px 2px 0 rgba(255,255,255,.2) inset,0 2px 10px 0 rgba(255,255,255,.5) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
+  border: 1px solid rgba(0,0,0,.2);
 `;
 
 const GageWrapper = styled.div`
