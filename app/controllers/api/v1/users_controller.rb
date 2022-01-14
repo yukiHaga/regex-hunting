4d9ff64569ec5ee99e3ec27c2d66e86a61868661
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-14 22:59:55 +0900

    Add: 普通にユーザー登録した場合、release_titlesテーブルにデータが追加されるように設定した

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,12 +3,18 @@ class Api::V1::UsersController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :create
   after_action :set_csrf_token_header, only: :create
 
+  # user.saveが成功したら、release_titlesのレコードも作る。
   def create
     user = User.new(user_params)
     raise ActiveRecord::RecordNotFound, user.errors.full_messages unless user.valid?
     if user.save
       reset_session
       auto_login(user)
+      current_user.release_titles.build(
+        release_date: Date.today,
+        title_id: Title.find_by(name: current_user[:active_title])[:id]
+      )
+      current_user.save!
       render json: {
         session: true,
         user: {
