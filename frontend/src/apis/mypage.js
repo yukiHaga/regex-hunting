12b23e50fe946e433188a6bfcaab56b0ef4e0e82
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-15 12:58:20 +0900

    Add: api関数の分割代入を修正した

--- frontend/src/apis/mypage.js
+++ frontend/src/apis/mypage.js
@@ -4,7 +4,7 @@ import { myPages } from '../urls/index';
 // マイページ情報を取得するAPIコール関数
 // postの第3引数にwithCredentials: trueを指定することで、
 // API(Rails)と通信する際にデータにcookieを含めることができる
-export const getMyPageInfo = async ({ user: { id } }) => {
+export const getMyPageInfo = async ({ id }) => {
   try {
     const response = await axios.get(
       myPages(id),
