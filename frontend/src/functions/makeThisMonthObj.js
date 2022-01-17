03ebab4d95fd61d15c6c051347950b5f7aff1189
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-17 23:42:23 +0900

    Fix: ヒートマップの時刻を日本時間にするように設定した

--- frontend/src/functions/makeThisMonthObj.js
+++ frontend/src/functions/makeThisMonthObj.js
@@ -7,7 +7,15 @@ export const makeThisMonthObj = (
 
   const month_obj = {};
 
-  for(const day = first_day; day <= end_day; day.setDate(day.getDate() + 1)) {
+  // UTC(世界の標準時間)と日本時間の差分を分で取得する
+  // 日本時間はUTCより9時間進んでいるので、-540が戻り値になる
+  const offset = first_day.getTimezoneOffset();
+
+  const add_first_day = new Date(first_day.getTime() - (offset * 60 * 1000));
+
+  const add_end_day = new Date(end_day.getTime() - (offset * 60 * 1000));
+
+  for(const day = add_first_day; day <= add_end_day; day.setDate(day.getDate() + 1)) {
     const formatDate = day.toISOString().slice(0, 10);
     
     month_obj[formatDate] = 0;
