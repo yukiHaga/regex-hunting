ba208d81db51d04c6a56cd65c8fd6ea34bbd31f2
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-19 21:17:20 +0900

    Add: マスクと鍵マークを解放していないタイトルに配置した

--- frontend/src/components/Games/TitleCard.jsx
+++ frontend/src/components/Games/TitleCard.jsx
@@ -4,6 +4,12 @@ import styled from 'styled-components';
 // DescriptionWrapper
 import { DescriptionWrapper } from '../../components/shared_style.js';
 
+// Colors
+import { COLORS } from '../../style_constants.js';
+
+// 鍵のアイコン
+import LockIcon from '@mui/icons-material/Lock';
+
 const InnerTitleCardWrapper = styled.div`
   width: 36%;
   height: 69px;
@@ -12,6 +18,26 @@ const InnerTitleCardWrapper = styled.div`
   margin: 20px;
   margin-left: 0;
   margin-right: 0;
+  position: relative;
+`;
+
+const MaskTitleCardWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  background-color: rgba(0,0,0,0.3);
+`;
+
+const LockWrapper = styled.div`
+  font-size: 40px;
+  position: absolute;
+  z-index: 2;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const TitleCardSentenceWrapper = styled(DescriptionWrapper)`
@@ -26,13 +52,23 @@ export const TitleCard = memo(({
   name,
   release_date
 }) => {
-
-  console.log(name);
   return (
     <>
       <InnerTitleCardWrapper>
+        {
+          !release_date &&
+            <>
+              <MaskTitleCardWrapper />
+              <LockWrapper>
+                <LockIcon
+                  fontSize='inherit' 
+                  sx={{ color: `${COLORS.BLACK}` }}
+                />
+              </LockWrapper>
+            </>
+        }
         <TitleCardSentenceWrapper>
-          学習カレンダー
+          {name}
         </TitleCardSentenceWrapper>
       </InnerTitleCardWrapper>
     </>
