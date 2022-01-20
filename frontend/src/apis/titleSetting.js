9c4a5558487d05848991e4f98ee0ed23597f9a67
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 11:59:11 +0900

    Add: titleSetting.jsを作成した

---
+++ frontend/src/apis/titleSetting.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { titleSettings } from '../urls/index';
+
+// マイページ情報を取得するAPIコール関数
+// postの第3引数にwithCredentials: trueを指定することで、
+// API(Rails)と通信する際にデータにcookieを含めることができる
+export const postTitleSetting = async ({ id, title }) => {
+  try {
+    const response = await axios.post(
+      titleSettings(id),
+      {
+        user: {
+          select_title: title
+        }
+      },
+      { withCredentials: true }
+    );
+    axios.defaults.headers.common['X-CSRF-Token'] = response.headers['x-csrf-token'];
+    return response.data;
+  } catch(e) {
+    throw e;
+  }
+};
