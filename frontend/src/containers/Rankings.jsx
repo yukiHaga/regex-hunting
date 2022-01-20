132d146cbdf7527dacc6e7991f476adcac5c97a3
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 22:55:15 +0900

    Add: ランキングページのレイアウトを整えた

--- frontend/src/containers/Rankings.jsx
+++ frontend/src/containers/Rankings.jsx
@@ -132,10 +132,7 @@ const HunterDataTd = styled.td`
 `;
 
 const CustomThead = styled.thead`
-  background-image: -webkit-linear-gradient(rgba(255,255,255,.3) 0%,transparent 50%,transparent 50%,rgba(0,0,0,.1) 100%);
-  background-image:         linear-gradient(rgba(255,255,255,.3) 0%,transparent 50%,transparent 50%,rgba(0,0,0,.1) 100%);
-  box-shadow: 0 2px 2px 0 rgba(255,255,255,.1) inset,0 2px 10px 0 rgba(255,255,255,.2) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
-  border: 1px solid rgba(0,0,0,.2);
+  box-shadow: 0 1px 2px rgba(0,0,0,0.3);
 `;
 
 export const Rankings = () => {
