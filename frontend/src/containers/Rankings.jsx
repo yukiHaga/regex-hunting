22d93d73bc15bd6058a9b81f14a9e518dcdb4d2d
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 20:46:40 +0900

    Add: ランキングページに初級編という文字をつけた

--- frontend/src/containers/Rankings.jsx
+++ frontend/src/containers/Rankings.jsx
@@ -11,9 +11,46 @@ import { UserContext } from "../context/UserProvider.js";
 // Colors
 import { COLORS } from '../style_constants.js';
 
+// DescriptionWrapper
+import { DescriptionWrapper } from '../components/shared_style.js';
+
+// NextButton
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+
+// PrevButton
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
+
 const FakeBlock = styled.div`
   background-color: ${COLORS.SUB};
   height: 56px;
+`;
+
+// メインのラッパー
+const MainWrapper = styled.div`
+  background-color: ${COLORS.SUB};
+`;
+
+const TitleLineWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
+
+const ButtonWrapper = styled.div`
+  font-size: 60px;
+  cursor: pointer;
+  :hover {
+    opacity: 0.7;
+  }
+  :focus {
+    outline: 0;
+  }
+`;
+
+const ChangeGraphBoxSentenceWrapper = styled(DescriptionWrapper)`
+  font-weight: bold;
+  font-size: 36px;
+  line-height: 54px;
+  text-align: center;
 `;
 
 export const Rankings = () => {
@@ -47,6 +84,27 @@ export const Rankings = () => {
       <Header /> 
       <FakeHeader />
       <FakeBlock />
+      <MainWrapper>
+        <TitleLineWrapper>
+          <ButtonWrapper 
+          >
+            <ArrowLeftIcon
+              fontSize='inherit' 
+              sx={{ color: `${COLORS.BLACK}` }}
+            />
+          </ButtonWrapper>
+          <ChangeGraphBoxSentenceWrapper>
+            初級編
+          </ChangeGraphBoxSentenceWrapper>
+          <ButtonWrapper
+          >
+            <ArrowRightIcon
+              fontSize='inherit' 
+              sx={{ color: `${COLORS.BLACK}` }}
+            />
+          </ButtonWrapper>
+        </TitleLineWrapper>
+      </MainWrapper>
     </>
   );
 };
