e5ba7c5ed05049465ed54de7f7c6d74ffd5bab0e
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 21:42:45 +0900

    Add: startボタンに光沢をつけた

--- frontend/src/components/Buttons/StartButton.jsx
+++ frontend/src/components/Buttons/StartButton.jsx
@@ -16,6 +16,10 @@ const StartButtonWrapper = styled(RedRoundButton)`
   :hover {
     box-shadow: 0 0 2px rgba(0,0,0,0.2);
   }
+  background-image: -webkit-linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  background-image:         linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  box-shadow: 0 2px 2px 0 rgba(255,255,255,.1) inset,0 2px 10px 0 rgba(255,255,255,.2) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
+  border: 1px solid rgba(0,0,0,.2);
 `;
 
 const StartButtonTextWrapper = styled.div`
