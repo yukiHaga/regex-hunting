14fc13cea5f5d8087ca791bd4ee958204b96484f
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 22:15:40 +0900

    Add: 説明モーダルのスタートボタンに光沢をつけた

--- frontend/src/components/Buttons/FinallyGameStartButton.jsx
+++ frontend/src/components/Buttons/FinallyGameStartButton.jsx
@@ -8,7 +8,6 @@ const FinallyGameStartButtonWrapper = styled.div`
   margin-top: 30px;
   border-style: none;
   border-radius: 3px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.3);
   transition: 0.3s;
   :hover {
     box-shadow: 0 0 2px rgba(0,0,0,0.2);
@@ -32,6 +31,10 @@ const FinallyGameStartButtonTextWrapper = styled.div`
   background-color: ${COLORS.BLUE};
   padding-top: 5px;
   padding-bottom: 5px;
+  background-image: -webkit-linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  background-image:         linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  box-shadow: 0 2px 2px 0 rgba(255,255,255,.1) inset,0 2px 10px 0 rgba(255,255,255,.2) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
+  border: 1px solid rgba(0,0,0,.2);
 `;
 
 export const FinallyGameStartButton = ({ 
