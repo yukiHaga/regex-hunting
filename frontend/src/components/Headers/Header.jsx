dc2cdb173ea70d1f3bff4df546094ee73615afaf
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 21:48:24 +0900

    Add: ヘッダーに光沢をつけた

--- frontend/src/components/Headers/Header.jsx
+++ frontend/src/components/Headers/Header.jsx
@@ -33,6 +33,10 @@ const HeaderWrapper = styled.div`
   box-shadow: 0 3px 6px rgba(0,0,0,0.2);
   position: fixed;
   z-index: 1;
+  background-image: -webkit-linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  background-image:         linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  box-shadow: 0 2px 2px 0 rgba(255,255,255,.1) inset,0 2px 10px 0 rgba(255,255,255,.2) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
+  border: 1px solid rgba(0,0,0,.2);
 `;
 
 const HeaderTitleImage = styled.img`
