06798dff3fefa8550e40d2751adaf9dd49a6c27f
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-20 22:08:18 +0900

    Add: ゲームコンテンツに書いてあるスタートボタンに光沢をつけた

--- frontend/src/components/Buttons/GameStartButton.jsx
+++ frontend/src/components/Buttons/GameStartButton.jsx
@@ -9,7 +9,6 @@ import { COLORS } from '../../style_constants.js';
 const GameStartButtonWrapper = styled(BaseLink)`
   border-style: none;
   border-radius: 3px;
-  box-shadow: 0 2px 4px rgba(0,0,0,0.3);
   transition: 0.3s;
   :hover {
     box-shadow: 0 0 2px rgba(0,0,0,0.2);
@@ -28,6 +27,10 @@ const GameStartButtonTextWrapper = styled.div`
   font-style: normal;
   font-weight: 500;
   line-height: 40px;
+  background-image: -webkit-linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  background-image:         linear-gradient(rgba(255,255,255,.3) 0%,transparent 30%,transparent 30%,rgba(0,0,0,.1) 100%);
+  box-shadow: 0 2px 2px 0 rgba(255,255,255,.1) inset,0 2px 10px 0 rgba(255,255,255,.2) inset,0 -2px 2px 0 rgba(0,0,0,.1) inset;
+  border: 1px solid rgba(0,0,0,.2);
 `;
 
 export const GameStartButton = ({difficulty}) => {
