932b05063ecd15a657e866ff3dd66ba4ccaaab9b
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-21 16:59:43 +0900

    Add: APIのURLに環境変数を設定した

--- frontend/src/urls/index.js
+++ frontend/src/urls/index.js
@@ -1,4 +1,4 @@
-const DEFAULT_API_URL = 'http://localhost:3000/api/v1';
+const DEFAULT_API_URL = `${process.env.REACT_APP_SERVER_URL}/api/v1`;
 const GITHUB_APP_ID = '420fbaef8a45fb65c5a9';
 const GITHUB_REDIRECT_URL = 'http://localhost:3001/callback/github/';
 const GOOGLE_APP_ID = '63383229438-hjv5021k3b43jjkf182772mg9mh6uvp9.apps.googleusercontent.com';
