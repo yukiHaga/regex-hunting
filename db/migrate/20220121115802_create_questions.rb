8dcb6e8ae9afe13cc3268aa103cc343a6eb13fe9
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-21 21:01:27 +0900

    Add: Questionsテーブルに対応したマイグレーションファイルを作成した

---
+++ db/migrate/20220121115802_create_questions.rb
@@ -0,0 +1,14 @@
+class CreateQuestions < ActiveRecord::Migration[6.0]
+  def change
+    create_table :questions do |t|
+      t.text :sentence, null: false
+      t.string :target_sentence, null: false
+      t.string :sample_answer, null: false
+      t.text :hint, null: false
+      t.text :commentary, null: false
+      t.integer :difficulty, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
