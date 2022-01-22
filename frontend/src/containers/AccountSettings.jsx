e962ead61c5d7ce86233ebb1bd655109ba092139
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-22 10:17:56 +0900

    Add: AccountSettings.jsx内のロジックを80%作成した

--- frontend/src/containers/AccountSettings.jsx
+++ frontend/src/containers/AccountSettings.jsx
@@ -1,7 +1,33 @@
-import React, { Fragment, useEffect, useContext } from 'react';
+import React, { Fragment, useEffect, useLayoutEffect, useContext } from 'react';
+import styled from 'styled-components';
+
+// Presentational Components
+import { Header } from '../components/Headers/Header.jsx';
+import { FakeHeader } from '../components/Headers/FakeHeader.jsx';
+import { DescriptionWrapper } from '../components/shared_style.js';
+import { Footer } from '../components/Footers/Footer.jsx';
 
 // Contextオブジェクト
 import { UserContext } from "../context/UserProvider.js";
+
+// ログイン状態を確認するAPIコール関数
+import { checkLoginStatus } from '../apis/checkLoginStatus.js'; 
+
+// HTTP_STATUS_CODE
+import { HTTP_STATUS_CODE } from '../constants';
+
+// Colors
+import { COLORS } from '../style_constants.js';
+
+const FakeBlock = styled.div`
+  background-color: ${COLORS.SUB};
+  height: 56px;
+`;
+
+// メインのラッパー
+const MainWrapper = styled.div`
+  background-color: ${COLORS.SUB};
+`;
 
 export const AccountSettings = () => {
 
@@ -11,11 +37,44 @@ export const AccountSettings = () => {
   const { 
     requestUserState: { 
       sessionState,
+      userState: { user },
       battleAudioState
     },
     dispatch, 
     requestUserActionTyps
   } = useContext(UserContext);
+
+  // ブラウザをリロードしてもログイン状態を維持するためのuseEffect
+  useLayoutEffect(() => {
+    if(sessionState === false){
+      checkLoginStatus().then((data) => {
+        dispatch({
+          type: requestUserActionTyps.REQUEST_SUCCESS,
+          payload: {
+            session: data.session,
+            user: data.user,
+          }
+        });
+      }).catch((e) => {
+        if(e.response.status === HTTP_STATUS_CODE.NOT_FOUND){
+          dispatch({
+            type: requestUserActionTyps.REQUEST_FAILURE,
+            payload: {
+              errors: e.response.data.errors
+            }
+          });
+        } else {
+          throw e;
+        }
+      })
+    }
+  }, [
+    dispatch, 
+    sessionState,
+    requestUserActionTyps.REQUEST, 
+    requestUserActionTyps.REQUEST_SUCCESS,
+    requestUserActionTyps.REQUEST_FAILURE
+  ]);
 
   // ゲーム中のユーザーがトップページに戻ったときに
   // 音を消すuseEffect
@@ -31,7 +90,13 @@ export const AccountSettings = () => {
 
   return (
     <>
-      アカウント設定画面
+      <Header /> 
+      <FakeHeader />
+      <FakeBlock />
+      <MainWrapper>
+
+      </MainWrapper>
+      <Footer />
     </>
   );
 };
