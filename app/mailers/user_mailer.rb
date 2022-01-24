e60d4c2b816fab0851c19487f8d61c707ebe8b6c
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-24 21:11:23 +0900

    add: リセットパスワード用のメーラーを作成した

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,13 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password_email.subject
+  #
+  def reset_password_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
