e51d3e9bec68c7462edddc181a999daf30f7a95b
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-26 15:55:22 +0900

    Add: アップバーのレイアウトを整えた

--- frontend/src/components/Headers/FakeHeader.jsx
+++ frontend/src/components/Headers/FakeHeader.jsx
@@ -1,21 +1,142 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const FakeHeaderWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  height: 55px;
-`;
+// Colors
+import { COLORS } from '../../style_constants.js';
 
-// LPページの場合、onClickLinkはモーダル管理のstateを更新する関数
-// ログインしている場合、onClickLinkは何もない。
-export const FakeHeader = ({onClickLink}) => {
+// useNavigate
+import { useNavigate } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+// デフォルトのアバター画像
+import DefaultAvatarImage from '../../images/default_avatar.png';
+
+// FakeHeaderもちゃんとレスポンシブにする
+export const FakeHeader = () => {
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <>
-      <FakeHeaderWrapper>
-      </FakeHeaderWrapper>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+            >
+              Regex Hunting
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: 'block', md: 'none' },
+                }}
+              >
+                <MenuItem key="1" onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">ログイン</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+            >
+              Regex Hunting
+            </Typography>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+              <Button
+                key="1"
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                ログイン
+              </Button>
+            </Box>
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="Remy Sharp" src={DefaultAvatarImage} />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
+              >
+                <MenuItem key="ログイン" onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">ログイン</Typography>
+                </MenuItem>
+              </Menu>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </>
   );
 };
--- frontend/src/components/Headers/Header.jsx
+++ frontend/src/components/Headers/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import styled from 'styled-components';
 
 // アイコン付きメニュー
@@ -21,6 +21,21 @@ import { HTTP_STATUS_CODE } from '../../constants';
 
 // useNavigate
 import { useNavigate } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+
+// デフォルトのアバター画像
+import DefaultAvatarImage from '../../images/default_avatar.png';
 
 const HeaderWrapper = styled.div`
   display: flex;
@@ -84,6 +99,10 @@ const HeaderNavFakeLink = styled(FakeLink)`
   }
 `;
 
+const ColorDiv = styled.div`
+  background-color: ${COLORS.MAIN};
+`;
+
 // LPページの場合、onClickLinkはモーダル管理のstateを更新する関数
 // ログインしている場合、onClickLinkは何もない。
 export const Header = ({
@@ -127,45 +146,120 @@ export const Header = ({
       }
     });
   };
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
   return (
     <>
-      <HeaderWrapper>
-        <HeaderTitleLink to={`/`}>
-          <TitleWrapper>
-            <Fuchiue>
-              Regex Hunting
-            </Fuchiue>
-            Regex Hunting
-          </TitleWrapper>
-        </HeaderTitleLink>
-        <HeaderNav>
-          <HeaderNavLink to={`/rankings`}>
-            ランキング
-          </HeaderNavLink>
-          {
-            sessionState === false && onClickLink && 
-              <>
-                <HeaderNavFakeLink onClick={() => onClickLink("login")}>
+      <ColorDiv>
+        <AppBar position="fixed" color='inherit'>
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+              >
+                Regex Hunting
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleOpenNavMenu}
+                  color="inherit"
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorElNav}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'left',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                  }}
+                  open={Boolean(anchorElNav)}
+                  onClose={handleCloseNavMenu}
+                  sx={{
+                    display: { xs: 'block', md: 'none' },
+                  }}
+                >
+                  <MenuItem key="1" onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">ログイン</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+              <Typography
+                variant="h6"
+                noWrap
+                component="div"
+                sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+              >
+                Regex Hunting
+              </Typography>
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Button
+                  key="1"
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
                   ログイン
-                </HeaderNavFakeLink>
-                <HeaderNavFakeLink onClick={() => onClickLink("signUp")}>
-                  無料会員登録
-                </HeaderNavFakeLink>
-              </>
-          }
-          { 
-            sessionState && userState && 
-              <>
-                <HeaderNavFakeLink>
-                  <IconMenu 
-                    handleLogout={handleLogout}
-                  />
-                </HeaderNavFakeLink>
-              </>
-          }
-        </HeaderNav>
-      </HeaderWrapper>
+                </Button>
+              </Box>
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Open settings">
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar alt="Remy Sharp" src={DefaultAvatarImage} />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: '45px' }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem key="ログイン" onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">ログイン</Typography>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      </ColorDiv>
     </>
   );
 };
