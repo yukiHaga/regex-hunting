55a4ee4b9dc89b8c1c7c237eb140229c5ea27ff6
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-27 23:29:42 +0900

    Remove: returnTop.jsを削除した

--- frontend/src/functions/returnTop.js
+++
@@ -1,6 +0,0 @@
-// スマホからコンテンツを利用した場合、トップページへ戻す関数
-export const returnTop = (navigate) => {
-  console.log("関数の中");
-  console.log(navigate);
-  return navigate('/', { state: { display: true, success: "PCからご利用ください。"}});
-};
