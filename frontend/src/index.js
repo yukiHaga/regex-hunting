4e5ded03ab9577b5c27d559ee52c730e93903a80
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-28 19:46:26 +0900

    Add: StyledEngineProviderをインポートシタ

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+      <App />
+    </StyledEngineProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
