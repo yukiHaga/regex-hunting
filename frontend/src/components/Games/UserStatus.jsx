92d3631f6a21bc7b70876907dd18ad73b550f702
yukiHaga <oceansthirteen7510@gmail.com>
2022-01-28 22:04:46 +0900

    useStatus.jsxをレスポンシブにした

--- frontend/src/components/Games/UserStatus.jsx
+++ frontend/src/components/Games/UserStatus.jsx
@@ -15,57 +15,27 @@ import DefaultAvatarImage from '../../images/default_avatar.png';
 
 // ステータスのラッパー
 const StatusWrapper = styled.div`
-  width: 700px;
-  height: 260px;
+  width: 49%;
   border-radius: 3px;
   background-color: ${COLORS.WHITE};
   box-shadow: 0 0px 20px rgba(0,0,0,0.2);
-  display: flex
-`;
-
-// イメージのラッパー
-const ImageWrapper = styled.div`
-  width: 220px;
-  height: 220px;
-  background-color: ${COLORS.WHITE};
   display: flex;
   align-items: center;
-  padding: 16px;
-`;
-
-// カスタムイメージ
-// imageタグだと縦横比を維持できない
-// background-imageだと縦横比を維持できる
-/*
-const CustomImage = styled.div`
-  background-image: url(${TemporaryUserImage});
-  width: 220px;
-  height: 220px;
-  border-radius: 50%;
-  background-size: cover;
-  background-position: center center;
-  border: 3px solid silver;
-  box-shadow: inset 1px 1px 3px 0 rgba(0, 0, 0, 0.8), 1px 1px 0 0 rgba(255, 255, 255, 0.12);
-`;
-*/
-
-const TableWrapper = styled.div`
-  align-self: center;
 `;
 
 const CustomTable = styled.table`
-  width: 430px;
+  width: 60%;
   border-collapse: collapse;
   color: ${COLORS.BLACK};
   font-family: YuGothic;
   font-weight: normal;
-  font-size: 18px;
-  margin: 0 auto;
+  font-size: 1.1em;
   border: none;
+  margin-left: 2%;
 `;
 
 const CustomTd = styled.td`
-  padding: 10px 40px; 
+  padding: 2% 3%; 
   border: none;
   text-align: right;
   border-bottom:solid 1px silver;
@@ -75,14 +45,12 @@ const NameTd = styled(CustomTd)`
   font-family: YuGothic;
   font-style: normal;
   font-weight: bold;
-  font-size: 32px;
-  line-height: 40px;
+  font-size: 2.0em;
   text-align: left;
   color: ${COLORS.BLACK};
 `;
 
 const MetaTd = styled(CustomTd)`
-  padding: 10px 40px; 
   border: none;
   text-align: left;
   border-bottom:solid 1px silver;
@@ -107,39 +75,35 @@ export const UserStatus = memo(({
   return (
     <>
       <StatusWrapper>
-        <ImageWrapper>
-          <Avatar
-            alt="Hunter"
-            src={image || DefaultAvatarImage}
-            sx={{ width: 200, height: 200 }}
-          />
-        </ImageWrapper>
-        <TableWrapper>
-          <CustomTable>
-            <tbody>
-              <tr>
-                <NameTd colSpan={2}>{name}</NameTd> 
-              </tr>
-              <tr>
-                <MetaTd>ランク</MetaTd> 
-                <CustomTd>{rank}</CustomTd>
-              </tr>
-              <tr>
-                <MetaTd>称号</MetaTd> 
-                <CustomTd>{active_title}</CustomTd>
-              </tr>
-              <tr>
-                <ExpTd colSpan={2}>
-                  <StatusExperienceBox 
-                    temporary_experience={temporary_experience}
-                    total_experience={total_experience}
-                    maximum_experience_per_rank={maximum_experience_per_rank}
-                  />
-                </ExpTd>
-              </tr>
-            </tbody>
-          </CustomTable>
-        </TableWrapper>
+        <Avatar
+          alt="Hunter"
+          src={image || DefaultAvatarImage}
+          sx={{ width: 200, height: 200, mx: '2%' }}
+        />
+        <CustomTable>
+          <tbody>
+            <tr>
+              <NameTd colSpan={2}>{name}</NameTd> 
+            </tr>
+            <tr>
+              <MetaTd>ランク</MetaTd> 
+              <CustomTd>{rank}</CustomTd>
+            </tr>
+            <tr>
+              <MetaTd>称号</MetaTd> 
+              <CustomTd>{active_title}</CustomTd>
+            </tr>
+            <tr>
+              <ExpTd colSpan={2}>
+                <StatusExperienceBox 
+                  temporary_experience={temporary_experience}
+                  total_experience={total_experience}
+                  maximum_experience_per_rank={maximum_experience_per_rank}
+                />
+              </ExpTd>
+            </tr>
+          </tbody>
+        </CustomTable>
       </StatusWrapper>
     </>
   );
