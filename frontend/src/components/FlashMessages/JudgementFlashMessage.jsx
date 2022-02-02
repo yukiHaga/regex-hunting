dffa1237a1899379103c18ca5b7e782ddabe2e80
yukiHaga <oceansthirteen7510@gmail.com>
2022-02-02 16:36:06 +0900

    Remove: フラッシュメッセージ内の使っていないコンポーネントを消した

--- frontend/src/components/FlashMessages/JudgementFlashMessage.jsx
+++ frontend/src/components/FlashMessages/JudgementFlashMessage.jsx
@@ -2,28 +2,11 @@ import React, { Fragment, useState, useEffect} from 'react';
 import styled from 'styled-components';
 
 // フラッシュメッセージ関係のコンポーネント;
-// import Slide from '@mui/material/Slide';
-
-// フラッシュメッセージ関係のコンポーネント;
 import Alert from '@mui/material/Alert';
 import Snackbar from '@mui/material/Snackbar';
 
 // Colors
 import { COLORS } from '../../style_constants.js';
-
-// handleTitleColorType
-// タイトルカラーを取り扱う関数
-import { handleTitleColorType } from '../../functions/handleTitleColorType.js'
-
-/*
-const CustomSlide = styled(Slide)`
-`;
-
-const JudgementFlashMessageWrapper = styled.div`
-  display: flex;
-  justify-content: end;
-`;
-*/
 
 const JudgementMessageTitle = styled.div`
   font-family: Raleway;
@@ -34,24 +17,6 @@ const JudgementMessageTitle = styled.div`
   padding-bottom: 1%;
   color: ${COLORS.SUB};
 `;
-
-/*
-const JudgementMessage = styled.div`
-  width: 190px;
-  height: 50px;
-  pointerEvents: 'none';
-  background-color: ${COLORS.SUB};
-  color: ${COLORS.BLACK};
-  font-family: YuGothic;
-  font-style: normal;
-  font-size: 16px;
-  font-weight: 500;
-  border-radius: 5px;
-  padding-left: 30px;
-  padding-right: 30px;
-  padding-bottom: 5px;
-`;
-*/
 
 export const JudgementFlashMessage = ({
   flash_display,
