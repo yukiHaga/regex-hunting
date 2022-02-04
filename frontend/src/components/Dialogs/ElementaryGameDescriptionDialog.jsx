e19ab0963d86bb2cb435b4042b07c6ff48ef3eac
yukiHaga <oceansthirteen7510@gmail.com>
2022-02-04 18:39:49 +0900

    Add: エレメンタリーのモーダルが80%完成した

--- frontend/src/components/Dialogs/ElementaryGameDescriptionDialog.jsx
+++ frontend/src/components/Dialogs/ElementaryGameDescriptionDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import styled, { keyframes } from 'styled-components';
 
 // ダイアログ
@@ -20,70 +20,11 @@ import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import { FinallyGameStartButton } from '../Buttons/FinallyGameStartButton.jsx';
 import { FinallyGameRestartButton } from '../Buttons/FinallyGameRestartButton.jsx';
 
-// MUIのツールチップコンポーネントを使う
-// チラツキがあるからやっぱりやめた
-// import Tooltip from '@mui/material/Tooltip';
+// ツールチップ
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
 
 /*
-// 画面全体にマスクを設置する設定
-// 画面の中央に要素を表示させる設定
-const MaskWrapper = styled.div`
-  position:fixed;
-  top:0;
-  left:0;
-  width:100%;
-  height:100%;
-  z-index: 1;
-  background-color:rgba(0,0,0,0.3);
-`;
-
-// PCの画面全体を表している
-const Carousel = styled.div`
-  z-index: 2;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  margin: 0 auto;
-  position: relative;
-  transition: right 0.5s ease;
-  right: ${({ widthState: { width } }) => `${width}px`};
-`;
-
-// 全てのスライドを包み込むラッパー
-// position: absoluteで全てのスライドを包み込むラッパーを、親要素を元にして固定しておく
-// 高さはpcの画面全体である
-const CarouselArea = styled.ul`
-  width: 4400px;
-  display: flex;
-  height: 100%;
-  align-items: center;
-  padding-left: 185px;
-  padding-right: 140px;
-  position: absolute;
-  margin-top: 0px;
-  margin-bottom: 0px;
-`;
-
-// 一枚あたりのスライドのラッパー
-// ここに移動ボタンが属している。
-const CarouselList = styled.li`
-  margin-right: 40px;
-  list-style: none;
-  background-color: ${COLORS.SUB};
-  width: 1000px;
-  height: 550px;
-  padding-top: 30px;
-  padding-left: 30px;
-  padding-right: 30px;
-  border-radius: 4px
-`;
-
-// 一枚あたりのスライド(ボタンは除く)
-// CarsolListの高さが550pxなので、ButtonLineWrapperの高さは自動的に150pxとなる
-const ModalWrapper = styled.div`
-  height: 450px;
-`;
-
 const WarningSentenceWrapper = styled.div`
   font-style: normal;
   font-weight: normal;
@@ -125,6 +66,12 @@ const CustomDialogInnerWrapper = styled.div`
   padding-bottom: 0;
 `;
 
+const DynamicSlideContentWrapper = styled.div`
+  width: 80%;
+  height: 78%;
+  margin: 0 auto;
+`;
+
 const TitleWrapper = styled.div`
   font-style: normal;
   font-weight: bold;
@@ -134,34 +81,24 @@ const TitleWrapper = styled.div`
   margin: 0 auto;
 `;
 
-const DynamicSlideContentWrapper = styled.div`
-  width: 80%;
-  height: 63%;
-  margin: 0 auto;
-`;
-
 // フェードアウトのアニメーション
 // transform: translateXは、x方向に動かすって意味
 // x方向の位置をどんどん原点に近づけることで、右から左に動いているように見える
-const FadeOutAnime = keyframes`
+const LeftSlideOutAnime = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  100% {
+    opacity: 0;
+    transform: translateX(-50%);
+  }
+`;
+
+const LeftSlideInAnime = keyframes`
   0% {
     opacity: 0;
-  }
-  20% {
-    opacity: 0;
-    transform: translateX(40%);
-  }
-  40% {
-    opacity: 0;
-    transform: translateX(30%);
-  }
-  60% {
-    opacity: 0.6;
-    transform: translateX(20%);
-  }
-  80% {
-    opacity: 0.8;
-    transform: translateX(10%);
+    transform: translateX(50%);
   }
   100% {
     opacity: 1;
@@ -169,13 +106,59 @@ const FadeOutAnime = keyframes`
   }
 `;
 
+const RightSlideOutAnime = keyframes`
+  0% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+  100% {
+    opacity: 0;
+    transform: translateX(50%);
+  }
+`;
+
+const RightSlideInAnime = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateX(-50%);
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+const slideFunction = (
+  slide_in,
+  slide_out,
+  direction
+) => {
+  switch (true){
+    case slide_out && !slide_in && direction === "left":
+      return LeftSlideOutAnime;
+    case !slide_out && slide_in && direction === "left":
+      return LeftSlideInAnime;
+    case slide_out && !slide_in && direction === "right":
+      return RightSlideOutAnime;
+    case !slide_out && slide_in && direction === "right":
+      return RightSlideInAnime;
+    default:
+      return false;
+  }
+};
+
+// animationプロパティは1つしか存在できない
+// 2個存在する場合、2個目で1個目が上書きされる
 const SlideContentWrapper = styled.div`
   width: 100%;
   height: 100%;
   margin: 0 auto;
-  opacity: ${({ right }) => right && "0"};
-  transform: ${({ right }) => right && "translateX(40%)"};
-  animation: ${({ right }) => right && FadeOutAnime } 0.7s ease-out forwards;
+  transform: translateX(0);
+  animation: ${({ 
+    slide_in, 
+    slide_out,
+    direction
+  }) => slideFunction(slide_in, slide_out, direction)} 0.7s ease forwards;
 `;
 
 const SentenceWrapper = styled.div`
@@ -194,16 +177,10 @@ const ButtonLineWrapper = styled.div`
 `;
 
 const ButtonWrapper = styled.div`
-  font-size: 5em;
-  height: auto;
-  cursor: pointer;
-  :hover {
-    opacity: 0.7;
-  }
   :focus {
     outline: 0;
   }
-  display: inline-block
+  display: inline-block;
 `;
 
 // isOpenはgameState.game_description_open
@@ -217,7 +194,10 @@ export const ElementaryGameDescriptionDialog = ({
   click_description_open
 }) => {
 
-  const slideContent = [
+  // 第2引数に空の依存配列を渡した場合、初回の1回のみ実行され、
+  // 2度目以降のレンダリング時にはキャッシュから値を取得する
+  // 最初のslide_numを0とした
+  const slideContent = useMemo(() => [
     {
       title: "初級編",
       sentence: "初級編を始める前に、正規表現とは何か?、どんなことができるのか？を学習しましょう。そして、初級編のゲームを通して、正規表現を作る際に使う基礎的なメタ文字をマスターしましょう！",
@@ -227,48 +207,96 @@ export const ElementaryGameDescriptionDialog = ({
       title: "What’s 正規表現？",
       sentence: "正規表現とは、複数の文字列からルールを見つけ出し、そのルールをパターンで表現したものです。正規表現を用いることで、文字列中に特定の文字が含まれているか判定できます。そして、含んでいる場合、どの位置にあるかを知ることができます。",
       slide_num: 1
+    },
+    {
+      title: "正規表現を作る上で重要なメタ文字",
+      sentence: "正規表現は、「文字列」と「メタ文字」で構成されています。そのため、メタ文字を知らないと正規表現を作ることができません。メタ文字とは、特殊な働きをする文字列です。メタ文字の例として、「[a-z]」というメタ文字は「a~zの中の一文字」を表します。また、「\\d」というメタ文字は0~9までの1つの数字を表します。",
+      slide_num: 2
+    },
+    {
+      title: "ルール説明",
+      sentence: "初級編では、重要なメタ文字を使った正規表現を学習していきます。時間内に正しい正規表現を入力すると、モンスターに攻撃できます。モンスターのHPを0にしたらゲームクリアです！",
+      slide_num: 3
     }
-  ];
+  ], []);
 
   const initialState = {
     title: slideContent[0]["title"],
     sentence: slideContent[0]["sentence"],
     slide_num: slideContent[0]["slide_num"],
-    left: false,
-    right: false
+    slide_in: false,
+    slide_out: false,
+    direction: "",
   };
 
   const [slideState, setSlideState] = useState(initialState);
 
-  // 右へずらす
-  const changeSlideToRight = () => {
-    setSlideState((prev) => ({
-      title: slideContent[prev.slide_num + 1]["title"],
-      sentence: slideContent[prev.slide_num + 1]["sentence"],
-      slide_num: slideContent[prev.slide_num + 1]["slide_num"],
-      left: false,
-      right: true
-    }));
-  };
+  // 右カーソルをクリックで左へずらす
+  // スライドが右のスライドになる
+  // slideState.slide_numが3より小さい場合、右カーソルが機能する
+  const changeSlideToRight = useCallback(() => {
+    if(slideState.slide_num < 3) {
+      setSlideState((prev) => ({
+        ...prev,
+        slide_in: false,
+        slide_out: true,
+        direction: "left"
+      }));
+      setTimeout(() => {
+        setSlideState((prev) => ({
+          ...prev,
+          title: slideContent[prev.slide_num + 1]["title"],
+          sentence: slideContent[prev.slide_num + 1]["sentence"],
+          slide_num: slideContent[prev.slide_num + 1]["slide_num"],
+          slide_in: true,
+          slide_out: false,
+          direction: "left",
+        }));
+      }, 350);
+    }
+  }, [
+    slideContent,
+    slideState.slide_num
+  ]);
 
-  // 左へ1095pxずらす
-  const changeSlideToLeft = () => {
-    setSlideState((prev) => ({
-      width: prev.count === 0 ? prev.width : prev.width - 1095,
-      count: prev.count === 0 ? prev.count : prev.count - 1
-    }));
-  };
+  // 左カーソルをクリックで右へずらす
+  // スライドが左のスライドになる
+  // slideState.slide_numが1より大きくないと、左カーソルが機能しないようにした
+  const changeSlideToLeft = useCallback(() => {
+    if(slideState.slide_num > 0) {
+      setSlideState((prev) => ({
+        ...prev,
+        slide_in: false,
+        slide_out: true,
+        direction: "right"
+      }));
+      setTimeout(() => {
+        setSlideState((prev) => ({
+          ...prev,
+          title: slideContent[prev.slide_num - 1]["title"],
+          sentence: slideContent[prev.slide_num - 1]["sentence"],
+          slide_num: slideContent[prev.slide_num - 1]["slide_num"],
+          slide_in: true,
+          slide_out: false,
+          direction: "right",
+        }));
+      }, 350);
+    }
+  }, [
+    slideContent,
+    slideState.slide_num
+  ]);
 
   useEffect(() => {
     if(game_description_open) {
       const handleRightkeyPress = (e) => {
-        if(e.key !== 'Enter' && e.key === 'ArrowRight') {
+        if(e.key !== 'Enter' && e.key === 'ArrowRight' && slideState.slide_num < 3) {
           changeSlideToRight();
         }
       };
 
       const handleLeftkeyPress = (e) => {
-        if(e.key !== 'Enter' && e.key === 'ArrowLeft') {
+        if(e.key !== 'Enter' && e.key === 'ArrowLeft' && slideState.slide_num > 0) {
           changeSlideToLeft();
         }
       };
@@ -286,7 +314,9 @@ export const ElementaryGameDescriptionDialog = ({
       }
     }
   }, [
-    game_description_open
+    game_description_open,
+    changeSlideToRight,
+    changeSlideToLeft
   ]);
 
   return(
@@ -302,24 +332,69 @@ export const ElementaryGameDescriptionDialog = ({
               pb: "0"
             }}
           >
-            <TitleWrapper>
-              {slideState.title}
-            </TitleWrapper>
             <DynamicSlideContentWrapper>
               <SlideContentWrapper
-                right={slideState.right}
+                slide_in={slideState.slide_in}
+                slide_out={slideState.slide_out}
+                direction={slideState.direction}
               >
+
+                <TitleWrapper>
+                  {slideState.title}
+                </TitleWrapper>
                 <SentenceWrapper>
                   {slideState.sentence}
                 </SentenceWrapper>
               </SlideContentWrapper>
             </DynamicSlideContentWrapper>
             <ButtonLineWrapper>
-              <ButtonWrapper onClick={changeSlideToRight}>
-                <ArrowRightIcon 
-                  fontSize='inherit' 
-                  sx={{ color: `${COLORS.BLACK}` }}
-                />
+              {
+                slideState.slide_num !== 0 &&
+                  <ButtonWrapper 
+                    onClick={changeSlideToLeft}
+                  >
+                    <Tooltip 
+                      title={<div>戻る<br />( 左矢印キー ← )</div>}
+                      placement="top"
+                    >
+                      <IconButton
+                        sx={{
+                          fontSize: '4.0em'
+                        }}
+                      >
+                        <ArrowLeftIcon
+                          fontSize='inherit' 
+                          sx={{ color: `${COLORS.BLACK}` }}
+                        />
+                      </IconButton>
+                    </Tooltip>
+                  </ButtonWrapper>
+              }
+              <ButtonWrapper 
+                onClick={changeSlideToRight}
+              >
+                <Tooltip 
+                  title={<div>進む<br />( 右矢印キー → )</div>}
+                  placement="top"
+                  disableHoverListener={slideState.slide_num === 3}
+                  sx={{
+                    opacity: slideState.slide_num === 3 ? 0 : 1,
+                  }}
+                >
+                  <IconButton
+                    sx={{
+                      fontSize: '4.0em',
+                      opacity: slideState.slide_num === 3 ? 0.1 : 1,
+                      cursor: slideState.slide_num === 3 && "default"
+                    }}
+                    disableRipple={slideState.slide_num === 3}
+                  >
+                    <ArrowRightIcon 
+                      fontSize='inherit' 
+                      sx={{ color: `${COLORS.BLACK}` }}
+                    />
+                  </IconButton>
+                </Tooltip>
               </ButtonWrapper>
             </ButtonLineWrapper>
           </DialogContent>
