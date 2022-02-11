a6849f5f5695d6d8c0cce62c1137e72201cd2a8e
yukiHaga <oceansthirteen7510@gmail.com>
2022-02-11 21:07:03 +0900

    Add: 今月の各難易度のゲームクリア回数を求めるメソッドを作成した

--- app/controllers/api/v1/my_pages_controller.rb
+++ app/controllers/api/v1/my_pages_controller.rb
@@ -8,7 +8,7 @@ class Api::V1::MyPagesController < ApplicationController
   def index
     game_frequencies_per_day = query_game_frequencies_per_day(@this_month)
     total_time_per_difficulty = query_total_time_per_difficulty(@this_month)
-    correct_avg_per_difficulty = query_correct_avg_per_difficulty(@this_month)
+    game_clear_count_per_difficulty = query_game_clear_count_per_difficulty(@this_month)
     fast_time_per_difficulty = query_fast_time_per_difficulty(@this_month)
     owned_titles = query_owned_titles
 
@@ -17,7 +17,7 @@ class Api::V1::MyPagesController < ApplicationController
     render json: {
       game_frequencies_per_day: game_frequencies_per_day,
       total_time_per_difficulty: total_time_per_difficulty,
-      correct_avg_per_difficulty: correct_avg_per_difficulty,
+      game_clear_count_per_difficulty: game_clear_count_per_difficulty,
       fast_time_per_difficulty: fast_time_per_difficulty,
       owned_titles: owned_titles
     }, status: :ok
@@ -59,32 +59,19 @@ class Api::V1::MyPagesController < ApplicationController
       end
     end
 
-    # subqueryはfromメソッドで使うサブクエリ
-    # 今月かつ各難易度の1ゲーム当たりの正解数を取得
-    # idとdifficultyでグループ化するので、そのグループ内のplay_dateは全て同じ値である
-    # こうすることで、日付に関わらず1ゲーム当たりの正解数を導出できる
-    # win lose関係なしに取得する
-    # correct_avg_per_difficultyは、今月の各難易度の平均正解数
-    # fromメソッドとサブクエリを組み合わせることで、
-    # fromの結果がcurrent_userの結果なので、current_userを前につけなくても大丈夫
-    # サブクエリの結果をテーブルとして使うことができる
-    # 難易度のグループ毎にAVG(correct)を計算する
-    # AVG(correct)によって、正解数の平均値が取得できる
-    # グループ毎にやると、グループ毎の正解数の平均値が取得できる
-    def query_correct_avg_per_difficulty(this_month)
-      subquery = current_user.game_managements
-                             .joins(:solved_questions)
-                             .where(
-                               play_date: this_month,
-                               solved_questions: { judgement: :correct }
-                             )
-                             .group(:id, :difficulty)
-                             .select('difficulty, COUNT(*) AS correct')
-      GameManagement.from("(#{subquery.to_sql}) AS results")
-                    .group(:difficulty)
-                    .select('difficulty, ROUND(AVG(correct)) AS correct_avg')
-                    .to_h do |data|
-        [data.difficulty, data.correct_avg]
+    # 今月の各難易度のゲームクリア回数
+    # 難易度がキー、今月の難易度におけるクリア回数がバリューになる
+    # キーバリューを3つ持つ1つのハッシュが作成される
+    def query_game_clear_count_per_difficulty(this_month)
+      current_user.game_managements
+                  .where(
+                    play_date: this_month,
+                    game_result: 'win'
+                  )
+                  .group(:difficulty)
+                  .select('difficulty, COUNT(*) AS total_count')
+                  .to_h do |data|
+        [data.difficulty, data.total_count]
       end
     end
 
