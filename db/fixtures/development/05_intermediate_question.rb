f51099e69765a02a61a6de6c46a25f832972fd4c
yukiHaga <oceansthirteen7510@gmail.com>
2022-02-25 14:54:36 +0900

    Add: 中級編の問題を全て作成した

--- db/fixtures/development/05_intermediate_question.rb
+++ db/fixtures/development/05_intermediate_question.rb
@@ -5,8 +5,8 @@ Question.seed(
     sentence: 'argument, arguementにマッチする正規表現を入力せよ。',
     target_sentence: 'argument arguement',
     sample_answer: 'argue?ment',
-    hint: '量指定子(「?」)と文字列を使用して、正規表現を作ってみましょう。',
-    commentary: '「e?」は、eまたはなしを表します。そのため、「argue?ment」でargument, arguementにマッチするような正規表現を表します。「argument|arguement」または「argu(m|em)ent」でマッチさせることもできます。',
+    hint: '文字列と量指定子(「?」)を使用して、正規表現を作ってみましょう。',
+    commentary: '「e?」は、eまたはなしを表します。そのため、「argue?ment」でargument, arguementにマッチするような正規表現を表します。「argument|arguement」または「argu(?:m|em)ent」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
 
@@ -16,8 +16,8 @@ Question.seed(
     sentence: 'environment, enviromentにマッチする正規表現を入力せよ。',
     target_sentence: 'environment enviroment',
     sample_answer: 'environ?ment',
-    hint: '量指定子(「?」)と文字列を使用して、正規表現を作ってみましょう。',
-    commentary: '「n?」は、nまたはなしを表します。そのため、「environ?ment」でenvironment, enviromentにマッチするような正規表現を表します。「environment|enviroment」または「enviro(nm|m)ent」でマッチさせることもできます。',
+    hint: '文字列と量指定子(「?」)を使用して、正規表現を作ってみましょう。',
+    commentary: '「n?」は、nまたはなしを表します。そのため、「environ?ment」でenvironment, enviromentにマッチするような正規表現を表します。「environment|enviroment」または「enviro(?:nm|m)ent」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
 
@@ -27,7 +27,7 @@ Question.seed(
     sentence: '<html>, <h>, <p>, <span>にマッチする正規表現を入力せよ',
     target_sentence: '<html> <h> <p> <span>',
     sample_answer: '<[a-z]+>',
-    hint: '文字クラスと量指定子(「+」)を使用して、正規表現を作ってみましょう。',
+    hint: '文字列, 文字クラス, 量指定子(「+」)を使用して、正規表現を作ってみましょう。',
     commentary: '「[a-z]+」はaからzの1文字が1文字以上を表します。そのため、「<[a-z]+>」で<html>, <h>, <p>, <span>にマッチするような正規表現を表します。 「<\\w+>」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
@@ -38,7 +38,7 @@ Question.seed(
     sentence: '<html>, <h>, <p>, <span>, <h5>, <h6>にマッチする正規表現を入力せよ。',
     target_sentence: '<html> <h> <p> <span> <h5> <h6>',
     sample_answer: '<[a-z]+[1-6]?>',
-    hint: '文字クラスと量指定子を使用して、正規表現を作ってみましょう。',
+    hint: '文字列, 文字クラス, 量指定子(「+」, 「?」)を使用して、正規表現を作ってみましょう。',
     commentary: '「[1-6]?」は、1から6の1桁の数字またはなしを表します。そのため、「<[a-z]+[1-6]?>」で<html>, <h>, <p>, <span>, <h5>, <h6>にマッチするような正規表現を表します。「<[a-z56]+>」または「<[a-z1-6]+>」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
@@ -48,9 +48,9 @@ Question.seed(
     id: 19,
     sentence: '<p>, <p class=\"red\">, <p class=\"blue\">にマッチする正規表現を入力せよ。',
     target_sentence: '<p> <p class=\"red\"> <p class=\"blue\">',
-    sample_answer: '<p( class=\"[a-z]+\")?>',
-    hint: '文字列, 括弧, 文字クラス, 量指定子を使用して、正規表現を作ってみましょう。',
-    commentary: '<p>と、属性値を持つ<p>がターゲットテキストです。属性値を持つまたは持たないという条件から、ターゲットテキストにマッチできる正規表現は、「<p(...)?>」という形であることが予想できます。class=\"...\"の部分は、「class=\"[a-z]+\"」で表すことができます。そのため、「<p( class=\"[a-z]+\")?>」で<p>, <p class=\"red\">, <p class=\"blue\">にマッチするような正規表現を表します。',
+    sample_answer: '<p(?: class=\"[a-z]+\")?>',
+    hint: '文字列, 括弧, 文字クラス, 量指定子(「+」, 「?」)を使用して、正規表現を作ってみましょう。',
+    commentary: '<p>と、属性値を持つ<p>がターゲットテキストです。属性値を持つまたは持たないという条件から、ターゲットテキストにマッチできる正規表現は、「<p(...)?>」という形であることが予想できます。class=\"...\"の部分は、「class=\"[a-z]+\"」で表すことができます。そのため、「<p(?: class=\"[a-z]+\")?>」で<p>, <p class=\"red\">, <p class=\"blue\">にマッチするような正規表現を表します。',
     difficulty: 'intermediate'
   },
 
@@ -72,7 +72,7 @@ Question.seed(
     target_sentence: '1 10 100 1000 10000 100000',
     sample_answer: '\\d{4,}',
     hint: '1桁の数字を表す特殊文字と量指定子(「{min,}」)を使用して、正規表現を作ってみましょう。',
-    commentary: '「\\d{4,}」で1000, 10000, 100000にマッチするような正規表現を表します。「[0-9]{4,}」または「1000(0*)」でマッチさせることもできます。',
+    commentary: '「\\d{4,}」で1000, 10000, 100000にマッチするような正規表現を表します。「[0-9]{4,}」または「1000(?:0*)」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
 
@@ -90,66 +90,66 @@ Question.seed(
   # 9問目
   {
     id: 23,
-    sentence: '3から8にマッチする正規表現を入力せよ。',
-    target_sentence: '0 1 2 3 4 5 6 7 8 9',
-    sample_answer: '[3-8]',
+    sentence: '3, |, Aにマッチする正規表現を入力せよ。',
+    target_sentence: '3 | A',
+    sample_answer: '[3|A]',
     hint: '文字クラスを使用して、正規表現を作ってみましょう。',
-    commentary: '[3-8]は3から８のどれか1つの数字を表します。',
+    commentary: '「[3|A]」は3, |, Aのどれか1つの文字を表します。そのため、正規表現「[3|A]」で3, |, Aにマッチさせることができます。注意点は、「|」を文字クラスの中で使用すると、ただの文字列として扱われることです。選択の意味はなくなります。',
     difficulty: 'intermediate'
   },
 
   # 10問目
   {
     id: 24,
-    sentence: '3から8, アルファベット, #にマッチする正規表現を入力せよ。',
-    target_sentence: '2 3 4 5 6 7 8 9 A B C D E F a b c d e f #',
-    sample_answer: '[3-8A-Fa-f#]',
-    hint: '文字クラス内で範囲を複数指定して、正規表現を作ってみましょう。',
-    commentary: '[3-8A-Fa-f#]は3から8, アルファベット, #のどれか1文字を表します。',
+    sentence: 'book, breakにマッチする正規表現を入力せよ。',
+    target_sentence: 'book break',
+    sample_answer: 'b(?:oo|rea)k',
+    hint: '文字列, 括弧, 選択を使用して、正規表現を作ってみましょう。',
+    commentary: 'bookとbreakの共通な文字列はb, kです。共通ではない文字列はooとreaです。ooとreaは2文字以上の単語なので、選択を使うと「oo|rea」と表せます。したがって、「b(?:oo|rea)k」はbook, breakにマッチする正規表現を表します。ちなみに、括弧を使わない場合、「boo|reak」という正規表現になります。この正規表現は、boo, reakにマッチする為、bookとbreakにはマッチしません。気をつけるべきことは、選択の優先順位は非常に低いということです。括弧を使用することで、選択の範囲を限定できます。そのため、解答では括弧を使用しました。余談ですが、「b[a-z]+k」でマッチさせることもできます。',
     difficulty: 'intermediate'
   },
 
   # 11問目
   {
     id: 25,
-    sentence: '6から8, ., アルファベットにマッチする正規表現を入力せよ。',
-    target_sentence: '6 7 8 . q r s Q R S',
-    sample_answer: '[6-8q-sQ-S.]',
-    hint: '文字クラス内で範囲を複数指定して、正規表現を作ってみましょう。',
-    commentary: '[6-8q-sQ-S.]は6から8, ., アルファベットのどれか1文字を表します。',
+    sentence: 'ck, cook, coook, cooookにマッチする正規表現を入力せよ。',
+    target_sentence: 'ck cook coook cooook',
+    sample_answer: 'co*k',
+    hint: '文字列と量指定子(「*」)を使用して、正規表現を作ってみましょう。',
+    commentary: 'ck, cook, coook, cooookの共通な文字列はc, kです。cとkの間のoの数が単語ごとに違います。任意の数のoは、「o*」で表すことができます。そのため、「co*k」でck, cook, coook, cooookにマッチする正規表現を表します。',
     difficulty: 'intermediate'
   },
 
   # 12問目
   {
     id: 26,
-    sentence: '1から3, アルファベット, #にマッチする正規表現を入力せよ。',
-    target_sentence: '1 2 3 A B C #',
-    sample_answer: '[1-3A-C#]',
-    hint: '文字クラスを使用して、正規表現を作ってみましょう。',
-    commentary: '[1-3A-C#]は1から3, アルファベット, #のどれか1文字を表します。',
+    sentence: '1, 112, 11212, 1121212, 112121212にマッチする正規表現を入力せよ。',
+    target_sentence: '1 112 11212 1121212 112121212',
+    sample_answer: '1(?:12)*',
+    hint: '文字列, 括弧, 量指定子(「*」)を使用して、正規表現を作ってみましょう。',
+    commentary: '全ての数は、先頭が1から始まります。その後、12が0回以上繰り返されています。そのため、「1(?:12)*」で1, 112, 11212, 1121212, 112121212にマッチする正規表現を表します。',
     difficulty: 'intermediate'
   },
 
   # 13問目
   {
     id: 27,
-    sentence: '#, %, @にマッチする正規表現を入力せよ。',
-    target_sentence: '# % @',
-    sample_answer: '[#%@]',
-    hint: '文字クラスを使用して、正規表現を作ってみましょう。',
-    commentary: '[#%@]は#, %, @のどれか1文字を表します。',
+    sentence: '-1.5, 0, 2, -123, -110.9にマッチする正規表現を入力せよ。',
+    target_sentence: '-1.5 0 2 -123 -110.9',
+    sample_answer: '-?\\d{1,3}(?:\.\\d)?',
+    hint: '「\\d」, 量指定子(「?」, 「{min,max}」)を使用して、正規表現を作ってみましょう。',
+    commentary: 'マイナスがあるかないかは「-?」で表すことができます。マイナスと小数点との間には1桁以上3桁以下の数が存在するので、「\\d{1,3}」で表すことができます。小数点と小数第一位の数があるかないかは「(?:\.\\d)?」で表すことができます。「.」自体は任意の1文字を表すので、純粋に.として使いたい場合、\\でエスケープします。したがって、「-?\\d{1,3}(?:\.\\d)?」で-1.5, 0, 2, -123, -110.9にマッチする正規表現を表します。',
     difficulty: 'intermediate'
   },
 
   # 14問目
   {
     id: 28,
-    sentence: 'sey以外の単語にマッチする正規表現を入力せよ。',
-    target_sentence: 'set sed sey sec',
-    sample_answer: 'se[^y]',
-    hint: '文字列と否定文字クラスを使用して、正規表現を作ってみましょう。',
-    commentary: '[^y]はy以外の1文字を表します。そのため、se[^y]でset, sed, secにマッチするような正規表現を表します。se[tdc]でマッチさせることもできます。',
+    sentence: 'Java, JavaScriptにマッチする正規表現を入力せよ。',
+    target_sentence: 'Java JavaScript Python',
+    sample_answer: 'Java(?:Script)?',
+    hint: '文字列, 括弧, 量指定子(「?」)を使用して、正規表現を作ってみましょう。',
+    commentary: 'Javaという文字列と、JavaScriptという文字列の違いは、Scriptという文字列があるかないかです。Scriptという文字列があるまたはなしは、(?:Script)?で表すことができます。そのため、「Java(?:Script)?」でJava, JavaScriptにマッチするような正規表現を表します。もし括弧をつけない場合、正規表現は「JavaScript?」となります。これは、JavaScript, JavaScripにマッチするだけであり、Javaにはマッチしません。「?」の対象はあくまで直前の1文字であるためです。括弧を使うことで、1文字以上の文字列を「?」の対象にすることができます。',
     difficulty: 'intermediate'
   },
 )
