c3699c13042dc200dc94f674f1714f8789dc9aec
yukiHaga <oceansthirteen7510@gmail.com>
2022-03-16 15:27:31 +0900

    Add: Sentryを導入した

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,12 @@
+Sentry.init do |config|
+  config.dsn = 'https://066c71526a474c319038e701f36eb1df:7c9b85877458470998c02656477663c0@o1168927.ingest.sentry.io/6261223'
+  config.breadcrumbs_logger = [:active_support_logger]
+
+  # To activate performance monitoring, set one of these options.
+  # We recommend adjusting the value in production:
+  config.traces_sample_rate = 0.5
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
