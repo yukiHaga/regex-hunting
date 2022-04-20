eb2c83469fd564ea7d134f266e11f0784f468a4a
yukiHaga <oceansthirteen7510@gmail.com>
2022-04-20 11:03:04 +0900

    Add: OAuthログイン成功時、ブラウザバックができないように設定した

--- frontend/src/containers/ExternalAuth.tsx
+++ frontend/src/containers/ExternalAuth.tsx
@@ -2,54 +2,50 @@ import React, { useEffect } from "react";
 import { useSearchParams, useNavigate, useParams } from "react-router-dom";
 
 // OAuth関係のAPIコール関数
-import { postExternalAuth } from '../apis/login';
+import { postExternalAuth } from "../apis/login";
 
-import { CircularMask } from '../components/loads/CircularMask';
+import { CircularMask } from "../components/loads/CircularMask";
 
 export const ExternalAuth = (): JSX.Element => {
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
 
-  const code = searchParams.get('code') as string;
-  const callBackState = searchParams.get('state');
+  const code = searchParams.get("code") as string;
+  const callBackState = searchParams.get("state");
   const { provider } = useParams();
 
   // ExternalAuthコンポーネントの初回レンダリング時に、
   // ユーザーをログインさせるかのアクションへリクエストを出す
   // catchメソッドを追加した
   useEffect(() => {
-    if (callBackState === 'xyz') {
+    if (callBackState === "xyz") {
       postExternalAuth({
         code,
-        provider
-      }).then((isSuccess) => {
-        if (isSuccess) {
-          navigate('/my-page', {
-            state: { display: true, success: "ログインしました。"}
+        provider,
+      })
+        .then((isSuccess) => {
+          if (isSuccess) {
+            navigate("/my-page", {
+              state: { display: true, success: "ログインしました。" },
+              replace: true,
+            });
+          } else {
+            navigate("/", {
+              state: { display: true, success: "ログインに失敗しました。" },
+            });
+          }
+        })
+        .catch((e) => {
+          navigate("/", {
+            state: { display: true, success: "ログインに失敗しました。" },
           });
-        } else {
-          navigate('/', {
-            state: { display: true, success: "ログインに失敗しました。"}
-          });
-        }
-      }).catch((e) => {
-        navigate('/', {
-          state: { display: true, success: "ログインに失敗しました。"}
         });
-      });
     } else {
-      navigate('/', {
-        state: { display: true, success: "ログインに失敗しました。"}
+      navigate("/", {
+        state: { display: true, success: "ログインに失敗しました。" },
       });
     }
-  }, [
-    callBackState,
-    code,
-    provider,
-    navigate
-  ]);
+  }, [callBackState, code, provider, navigate]);
 
-  return (
-    <CircularMask />
-  );
+  return <CircularMask />;
 };
