7ce3569d9c7e29749c58007c29d2fa5042a1bbe0
yukiHaga <oceansthirteen7510@gmail.com>
2022-05-03 12:45:51 +0900

    Add: ログをユーザーセッションに追加した

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,18 +6,28 @@ class Api::V1::UsersController < ApplicationController
 
   # user.saveが成功したら、release_titlesのレコードも作る。
   def create
+    # ログ1
+    logger.debug "①userの値: #{@user}"
     raise ActiveRecord::RecordNotFound, @user.errors.full_messages unless @user.valid?
 
     if @user.save
+      # ログ2
+      logger.debug "②if文true時のuser: #{@user}"
       reset_session
       auto_login(@user)
+      # ログ3
+      logger.debug "③if文true時のcurrent_user: #{current_user}"
       current_user.release_new_title(Title.find_by(name: current_user[:active_title])[:id])
+      # ログ4
+      logger.debug "④リリースタイトル付与時のcurrent_user: #{current_user}"
       current_user.save!
       render json: {
         session: true,
         user: User.handle_profile_user_serializer(current_user, current_user.avatar.attached? ? url_for(current_user.avatar) : nil)
       }, status: :created
     else
+      logger.debug "⑤if文false時のuser: #{@user}"
+      logger.debug "⑤if文false時のエラー内容: #{@user.errors.full_messages}"
       render json: { errors: @user.errors }, status: :bad_request
     end
   end
